8c5edac9d71166a3e3fdbe1b2f508c395c083345
Pedro Sousa <pedro.sousa@mercedes-benz.io>
2021-07-17 12:26:19 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./style.less";
+
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "Hello World";
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src")],
+        use: "ts-loader",
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: [MiniCssExtractPlugin.loader, "css-loader"],
+      // },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "less-loader",
+        ],
+      },
+    ],
+  },
+};
